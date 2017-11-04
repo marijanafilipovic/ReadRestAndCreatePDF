3c622ff7ff57adb548c3e7a910d589f802744d0b
Marijana <marijana.beograd@gmail.com>
2017-11-04 13:03:22 +0100

    PS-2017.1.4 <Marijana@Marijana-PC Overwrite remote C:\wamp\www\viselioApp to local

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Marijana
- * Date: 9/21/2017
- * Time: 8:56 PM
- */
-define('ROOT_URL', substr($_SERVER['PHP_SELF'], 0, - (strlen($_SERVER['SCRIPT_FILENAME']) - strlen(ROOT_DIR))));
